ae1351843370e6bb34ddc4d54c32e71872054b45
Administrator <Administrator@WIN-C5V8RGILFBC>
2021-01-24 11:25:51 -0600

    First Commit

---
+++ Java_practice/src/intro_class.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class intro_class {
+
+	int myAge;
+	double mySalary;
+	Scanner scan = new Scanner(System.in);
+public void myIntro() {
+	
+	
+	System.out.println("Please Enter the Your age");
+	myAge= scan.nextInt();
+	
+	System.out.println("Please enter the your salary");
+	
+	mySalary= scan.nextDouble();
+	
+	
+}
+	
+}
---
+++ Java_practice/src/my_Intro.java
@@ -0,0 +1,12 @@
+
+public class my_Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		intro_class i = new intro_class();
+		i.myIntro();
+
+	}
+
+}
